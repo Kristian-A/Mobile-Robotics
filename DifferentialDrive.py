13823a4037cd34e22c1540470e1c35814dcf68ef
Kristian Apostolov <kristian@pc-9-250.customer.ask4.lan>
2022-11-16 19:12:39 +0000

    split files

--- DifferentialDrive.py
+++ DifferentialDrive.py
@@ -1,73 +1,6 @@
 from __future__ import annotations
 import numpy as np
-
-class Point:
-    def __init__(self, *args) -> None:
-        if type(args[0]) is float or type(args[0]) is np.float_:
-            self.x = args[0]
-            self.y = args[1]
-            return
-        if type(args[0]) is Point or type(args[0]) is Pose:
-            other = args[0]
-            self.x = other.x
-            self.y = other.y
-            return
-    
-    def _rotate(self, theta: float) -> Point:
-        rotation_matrix = np.array([
-            [np.cos(theta), -np.sin(theta)],
-            [np.sin(theta), np.cos(theta)]
-        ])
-        result = rotation_matrix.dot(self.__tuple())
-        return Point(result[0], result[1])
-    
-    def __tuple(self) -> np.ndarray:
-        return (self.x, self.y)
-
-    def __mul__(self, object: float | Point) -> Point:
-        if type(object) is float:
-            return Point(self.x * object, self.y * object)
-        if type(object) is Point:
-            return Point(self.x * object.x, self.y * object.y)
-        raise Exception('Multiplication with Point requires Point or float')
-
-    def __add__(self, point: Point) -> Point:
-        return Point(self.x + point.x, self.y + point.y)
-
-    def __str__(self):
-        return f'({self.x}, {self.y})'
-    
-class Pose(Point):
-    def __init__(self, *args) -> None:
-        super().__init__(*args)
-        if type(args[0]) is float or type(args[0]) is np.float_:
-            self.theta = args[-1]
-            return
-        if type(args[0]) is Pose:
-            self.theta = pose.theta
-            return
-        if type(args[0]) is Point:
-            self.theta = args[-1]        
-            return
-
-    def rotate(self, rotation_angle: float, icr: Point) -> Pose:
-        translated_point = Point(self - icr)
-        rotated_point = translated_point._rotate(rotation_angle)
-        result_point = rotated_point + icr
-        return Pose(result_point, rotation_angle + self.theta)
-    
-    def __add__(self, object: Pose | Point) -> Pose:
-        if type(object) is Pose:
-            return Pose(self.x + object.x, self.y + object.y, self.theta + object.theta)
-        if type(object) is Point:
-            return Pose(self.x + object.x, self.y + object.y, self.theta) 
-        raise Exception('Addition with Pose requires another Pose or Point')
-
-    def __sub__(self, point: Point) -> Pose :
-        return Pose(self.x - point.x, self.y - point.y, self.theta)
-
-    def __str__(self):
-        return f'({self.x}, {self.y}, {self.theta})'
+from coordinates import *
 
 class DifferentialDrive:  
     def get_icr(self, pose: Pose, icr_radius: float) -> Pose:
@@ -100,11 +33,15 @@ class DifferentialDrive:
 diff_drive = DifferentialDrive()
 
 pose = Pose(6.0, 2.0, np.radians(120))
+pose2 = Pose(6.0, 3.0, np.radians(120))
+point = Point(7.0, 2.0)
 
-icr_radius = diff_drive.get_radius(3*np.pi, 2*np.pi, 4)
-print(icr_radius)
-icr = diff_drive.get_icr(pose, icr_radius)
-angular_velocity = diff_drive.get_angular(3*np.pi, 2*np.pi, 4)
-final_pose = pose.rotate(angular_velocity*2, icr)
+print(pose + pose2)
 
-print(final_pose)
+# icr_radius = diff_drive.get_radius(3*np.pi, 2*np.pi, 4)
+# print(icr_radius)
+# icr = diff_drive.get_icr(pose, icr_radius)
+# angular_velocity = diff_drive.get_angular(3*np.pi, 2*np.pi, 4)
+# final_pose = pose.rotate(angular_velocity*2, icr)
+
+# print(final_pose)
