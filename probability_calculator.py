60fdc5d904a2bb6f2a264397315d995d1a3af5b1
Tin Dizdarevic <tinovmail@gmail.com>
2022-11-17 19:00:39 +0000

    Change file names.

--- probability_calculator.py
+++ probability_calculator.py
@@ -27,36 +27,20 @@ class State:
             total += state.probability * probability
         return total
 
-# P(x|y) = P(y|x_1)P(x_1) / (p(y|x_1)*p(x_1) + p(y|x_2)p(x_2))
-marginal_space = MarginalSpace()
-marginal_space_element1 = marginal_space.add_conditional_probability(0.7, 0.2)
-marginal_space_element2 = marginal_space.add_conditional_probability(0.3, 0.8)
-
-# P(state=clean | u, state=dirty) = 0.8 * P(state = dirty) = 0.8 * 0.6
-# P(state=clean | u, state=clean) = 1.0 * P(state = clean) = 1.0 * 0.4
-# P(state=dirty | u, state=dirty) = 0.2 * P(state = dirty) = 0.2 * 0.6
-# P(state=dirty | u, state=clean) = 0.0 * P(state = clean) = 0.0 * 0.4
-# P(state=clean | u) = P()
-
+# P(state=clean | u, state=dirty) = 0.8, P(state = dirty) = 0.6
+# P(state=clean | u, state=clean) = 1.0, P(state = clean) = 0.4
+# P(state=clean | u) = P(state=clean | u, state=dirty) *  P(state = dirty) + P(state=clean | u, state=clean) * P(state = clean)
+# P(state=clean | u) = 0.8 * 0.6 + 1.0 * 0.4
 clean = State(0.4)
 dirty = State(0.6)
 # The probability of clean given previous was dirty is 0.8
 clean.given_previous_was(dirty, 0.8)
 clean.given_previous_was(clean, 1)
-dirty.given_previous_was(dirty, 0.2)
-dirty.given_previous_was(clean, 0)
+print(clean.estimate())
 
-clean.estimate()
-dirty.estimate()
-# P(s=y | t=y) = 0.7
-# P(s=y | t=w) = 0.3
-# P(t=y) = 0.2
-# P(t=w) = 0.8
-
-# P(t=y | s=y) = P(s=y|t=y) * P(t=y) / P(s=y|)
-
-ms = MarginalSpace()
-
-ms.add_conditional_probability(0.7, 0.2)
-ms.add_conditional_probability(0.3, 0.8)
-print(ms.get_probabilities())
+# Bayes Theorem Solver:
+# P(x|y) = P(y|x_1)P(x_1) / (p(y|x_1)*p(x_1) + p(y|x_2)p(x_2))
+marginal_space = MarginalSpace()
+marginal_space_element1 = marginal_space.add_conditional_probability(0.7, 0.2)
+marginal_space_element2 = marginal_space.add_conditional_probability(0.3, 0.8)
+marginal_space.get_probabilities()
